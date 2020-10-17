2836fac42a47b9ca9989cad89cb96059575118c7
Yony Calsin <helloyonicb@gmail.com>
2020-10-17 17:18:09 -0500

    fix: 🐛 fixes [skip ci]

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ const root = (...more) => path.resolve('scripts', 'database', ...more);
 
 module.exports = {
    env: 'development',
-   config: root('config', 'config.js'),
+   config: root('config', 'config.json'),
    'models-path': root('models'),
    'seeders-path': root('seeders'),
    'migrations-path': root('migrations'),
